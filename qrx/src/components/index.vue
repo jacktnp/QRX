ff33dd6c43cf31f4942e8c92d6b51f0ac38fb541
jacktnp <thanaponpkw@gmail.com>
2019-07-12 19:46:43 +0700

    :balloon: generated qrcode success

---
+++ qrx/src/components/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
+    </div>
+</template>
+
+<script>
+import QrcodeVue from 'qrcode.vue';
+ 
+export default {
+  data() {
+    return {
+      value: 'https://example.com',
+      size: 300
+    }
+  },
+  components: {
+    QrcodeVue
+  }
+}
+</script> 
--- qrx/src/router/index.js
+++ qrx/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import qrpage from '@/components/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hw',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'QRCode',
+      component: qrpage
     }
   ]
 })
